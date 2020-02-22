0add5753058b2f61131d829841fa37b9e3bbc781
Miranda Ward <Randa@Mirandas-MacBook-Pro.local>
2020-02-22 16:58:22 -0600

    Getting caught up for class

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -4,19 +4,31 @@ import javax.inject._
 
 import edu.trinity.videoquizreact.shared.SharedMessages
 import play.api.mvc._
+import swiftvis2.plotting._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import swiftvis2.plotting.renderer.JVMSVGInterface
+import swiftvis2.plotting.renderer.SwingRenderer
+import java.io.ByteArrayOutputStream
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
-    Ok(views.html.index(SharedMessages.itWorks))
+  def index = Action { implicit request =>
+    Ok(views.html.index("<b>Testing</b>"))//SharedMessages.itWorks))
   }
 
-  def tempPage = Action {
-    //Ok("<p>This is a page</p>").as("text/html")
-    Ok(views.html.tempPage())
+  def enterName = Action { implicit request =>
+    Ok(views.html.enterName(request.session.get("username")))
   }
-
-  def temps(month: int, year:Int) = TODO
+  
+  def rememberName(name: String) = Action { implicit request =>
+    Redirect(routes.Application.enterName).
+      withSession("username" -> name, "userid" -> "000")
+  }
+  
+  def forgetName = Action { implicit request =>
+    Redirect(routes.Application.enterName).withNewSession
+  }
 
 }
---
+++ server/app/models/TempModel.scala
@@ -0,0 +1,22 @@
+  
+package models
+
+case class TempData(day: Int, dayOfYear: Int, month: Int, stateId: String,
+  year: Int, precip: Double, tave: Double, tmin: Double, tmax: Double)
+
+object TempModel {
+  val data = readData()
+
+  def readData(): Seq[TempData] = {
+    val file = if (new java.io.File("data/SanAntonioTemps.csv").exists) "data/SanAntonioTemps.csv" else "../data/SanAntonioTemps.csv"
+    val source = scala.io.Source.fromFile(file)
+    val lines = source.getLines().drop(2)
+    val ret = lines.map { line =>
+      val p = line.split(",")
+      TempData(p(0).toInt, p(1).toInt, p(2).toInt, p(3), p(4).toInt,
+        p(5).toDouble, p(6).toDouble, p(7).toDouble, p(8).toDouble)
+    }.toArray
+    source.close()
+    ret
+  }
+}
